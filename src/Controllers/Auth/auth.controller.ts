8a520bf3b79ab8e04771ccd6fec241aa26538a87
csuftitan <naveengolla123@gmail.com>
2023-02-03 16:32:36 -0800

    paths fixed

--- src/Controllers/Auth/auth.controller.ts
+++ src/Controllers/Auth/auth.controller.ts
@@ -1,11 +1,10 @@
 import { Body, Controller, Post, UnauthorizedException, UseGuards } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
-import { AuthDto } from "src/dto/authDto";
-import { Users } from "src/Entities/Users.entity";
+import { AuthDto } from "../../dto/authDto";
+import { Users } from "../../Entities/Users.entity";
 import { Repository } from "typeorm";
 import { AuthService } from "./auth.service";
-import * as bcrypt from 'bcrypt'
 import { AuthGuard } from "@nestjs/passport";
 
 @Controller("Auth")
