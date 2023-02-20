2d28c084d546924717df321877d347693bfd2244
csuftitan <naveengolla123@gmail.com>
2023-02-19 21:40:46 -0800

    ejs added

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,9 @@ import { Category } from './Entities/Category.entity';
 import { Labels } from './Entities/Labels.entity';
 import { News } from './Entities/News.entity';
 import { Users } from './Entities/Users.entity';
+import * as path from 'path';
 import { SharedService } from './Shared/SharedService';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
 
 @Module({
   imports: [
@@ -23,6 +25,17 @@ import { SharedService } from './Shared/SharedService';
       entities : [News,Category,Labels,Users],
       synchronize: true
     }),
+    // I18nModule.forRoot({
+    //   fallbackLanguage: 'en',
+    //   loaderOptions: {
+    //     path: path.join(__dirname, '/i18n/'),
+    //     watch: true,
+    //   },
+    //   resolvers: [
+    //     { use: QueryResolver, options: ['lang'] },
+    //     AcceptLanguageResolver,
+    //   ]
+    // }),
     TypeOrmModule.forFeature([Users]),
     CategoryModule,
     LabelsModule,
